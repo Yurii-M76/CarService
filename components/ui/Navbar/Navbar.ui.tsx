498cc5be4745e8d342592a5d8294c44cdcc2a27f
Yurii M <ispoyar@yandex.ru>
2025-04-11 13:38:26 +0300

    create NavbarUI

--- components/ui/Navbar/Navbar.ui.tsx
+++ components/ui/Navbar/Navbar.ui.tsx
@@ -1,12 +1,44 @@
+"use client";
 import { FC } from "react";
 import classes from "./navbar.module.css";
+import Link from "next/link";
+import { Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 type TNavbar = {
   links: { path: string; label: string; accent: boolean }[];
 };
 
 const Navbar: FC<TNavbar> = ({ links }) => {
-  return "Navbar";
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <>
+      <nav className={classes.navbar}>
+        <ul className={classes.list}>
+          {links.map((link, index) => (
+            <li key={link.path} className={classes.item}>
+              <Link
+                href={link.path}
+                className={[classes.link, link.accent && classes.accent]
+                  .filter(Boolean)
+                  .join(" ")}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <Burger
+          className={classes.mobile}
+          opened={opened}
+          onClick={toggle}
+          aria-label="Toggle navigation"
+          color="var(--app-accent-color)"
+        />
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
