d29f9a2976200049f58760871a214a55a8bab5f9
Yurii M <ispoyar@yandex.ru>
2025-04-17 17:08:19 +0300

    added ContentUI, AdvantagesUI to components ui

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,9 +1,11 @@
 export { default as LogoUI } from "./Logo/Logo.ui";
 export { default as HeaderUI } from "./Header/Header.ui";
 export { default as NavbarUI } from "./Navbar/Navbar.ui";
+export { default as ContentUI } from "./Content/Content.ui";
 export { default as AboutUI } from "./About/About.ui";
 export { default as ServicesUI } from "./Services/Services.ui";
 export { default as ServiceCard } from "./ServiceCard/ServiceCard.ui";
+export { default as AdvantagesUI } from "./Advantages/Advantages.ui";
 export { default as BrandsUI } from "./Brands/Brands.ui";
 export { default as BrandCardUI } from "./BrandCard/BrandCard.ui";
 export { default as SectionUI } from "./Section/Section.ui";
