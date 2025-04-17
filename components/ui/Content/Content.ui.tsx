16f1b91bbe57905702445e4ff78f125cdb592fec
Yurii M <ispoyar@yandex.ru>
2025-04-17 17:07:09 +0300

    create ContentUI

---
+++ components/ui/Content/Content.ui.tsx
@@ -0,0 +1,80 @@
+import { FC, ReactNode } from "react";
+import Image from "next/image";
+import ImageWrapperUI from "../ImageWrapper/ImageWrapper.ui";
+import classes from "./content.module.css";
+
+type TContentUI = {
+  variant: "leftImage" | "rightImage";
+  title: string;
+  pathImage: string;
+  widthImage?: number;
+  heightImage?: number;
+  gapContent?: "sm" | "md" | "lg";
+  marginX?: boolean;
+  children?: ReactNode;
+};
+
+const ContentUI: FC<TContentUI> = ({
+  title,
+  pathImage,
+  children,
+  variant,
+  widthImage,
+  heightImage,
+  gapContent,
+  marginX,
+}) => {
+  const gap = gapContent && {
+    gap: {
+      sm: "var(--app-block-spacing-20)",
+      md: "var(--app-block-spacing-40)",
+      lg: "var(--app-block-spacing-60)",
+    }[gapContent],
+  };
+
+  const margin = marginX
+    ? {
+        margin: "var(--app-block-marginX-80)",
+      }
+    : undefined;
+
+  const image = (
+    <ImageWrapperUI width={widthImage} height={heightImage}>
+      <Image
+        src={pathImage}
+        alt={title}
+        width={500}
+        height={500}
+        className={classes.image}
+      />
+    </ImageWrapperUI>
+  );
+
+  const text = (
+    <div className={classes.content} style={{ ...gap }}>
+      <h2>{title}</h2>
+      {children && <div className={classes.paragraph}>{children}</div>}
+    </div>
+  );
+
+  const content =
+    variant === "leftImage" ? (
+      <>
+        {image}
+        {text}
+      </>
+    ) : (
+      <>
+        {text}
+        {image}
+      </>
+    );
+
+  return (
+    <div className={classes.container} style={margin}>
+      {content}
+    </div>
+  );
+};
+
+export default ContentUI;
