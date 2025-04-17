2f9451c15b589409b1824200f98aa984ad6c1a0f
Yurii M <ispoyar@yandex.ru>
2025-04-17 17:06:40 +0300

    modified ImageWrapper (added width & height props)

--- components/ui/ImageWrapper/ImageWrapper.ui.tsx
+++ components/ui/ImageWrapper/ImageWrapper.ui.tsx
@@ -3,10 +3,19 @@ import classes from "./imageWrapper.module.css";
 
 type TImageWrapperUI = {
   children: ReactNode;
+  width?: number;
+  height?: number;
 };
 
-const ImageWrapperUI: FC<TImageWrapperUI> = ({ children }) => {
-  return <div className={classes.imageWrapper}>{children}</div>;
+const ImageWrapperUI: FC<TImageWrapperUI> = ({ children, width, height }) => {
+  return (
+    <div
+      className={classes.imageWrapper}
+      style={{ inlineSize: `${width}px`, maxBlockSize: `${height}px` }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default ImageWrapperUI;
