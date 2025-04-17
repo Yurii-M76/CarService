07cbcfb501f526d8ed04f5deb58596c0b3014f04
Yurii M <ispoyar@yandex.ru>
2025-04-17 17:07:36 +0300

    refactoring AboutUI

--- components/ui/About/About.ui.tsx
+++ components/ui/About/About.ui.tsx
@@ -1,29 +1,18 @@
-import Image from "next/image";
-import { BrandsUI, ImageWrapperUI } from "..";
-import classes from "./about.module.css";
+import ContentUI from "../Content/Content.ui";
 
 const AboutUI = () => {
   return (
-    <div className={classes.about}>
-      <div className={classes.content}>
-        <ImageWrapperUI>
-          <Image
-            src="/about-image.jpg"
-            alt="image"
-            width={500}
-            height={500}
-            className={classes.image}
-          />
-        </ImageWrapperUI>
-        <div className={classes.group}>
-          <h2>Профессиональные механики с большим опытом работы</h2>
-          <p>
-            Узнайте цену ремонта своего авто за 1 минуту и получите скидку на
-            диагностику <strong>10%</strong>*
-          </p>
-        </div>
-      </div>
-    </div>
+    <ContentUI
+      variant="leftImage"
+      title="Профессиональные механики с большим опытом работы"
+      pathImage="/about-image.jpg"
+      marginX
+    >
+      <span>
+        Узнайте цену ремонта своего авто за 1 минуту и получите скидку на
+        диагностику <strong>10%*</strong>
+      </span>
+    </ContentUI>
   );
 };
 
