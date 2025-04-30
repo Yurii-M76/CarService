e2b69f92f5b83ecc716a9d2175cebf7b913e31c4
Yurii M <ispoyar@yandex.ru>
2025-04-30 15:20:39 +0300

    fixed Survey types

--- types/survey.ts
+++ types/survey.ts
@@ -1,16 +1,22 @@
-export type TSurveyItem = {
+import { ReactNode } from "react";
+
+type TSurveyItem = {
   label: string;
   values?: string[];
   description?: string;
 };
 
 export type TSurveyItems = {
-  items: {
-    problem: TSurveyItem;
-    release: TSurveyItem;
-    odometer: TSurveyItem;
-    model: TSurveyItem;
-    components: TSurveyItem;
-    contact: TSurveyItem;
-  };
+  problem: TSurveyItem;
+  release: TSurveyItem;
+  odometer: TSurveyItem;
+  model: TSurveyItem;
+  components: TSurveyItem;
+  contact: TSurveyItem;
+};
+
+export type TSurveyUI = {
+  title: string;
+  description: string;
+  children: ReactNode;
 };
