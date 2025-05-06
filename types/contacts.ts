16da6d3c2b494682f104e323a567ff25cc5e2f61
Yurii M <ispoyar@yandex.ru>
2025-05-06 12:19:44 +0300

    added contacts types

---
+++ types/contacts.ts
@@ -0,0 +1,9 @@
+export type TContacts = {
+  title: string;
+  content: {
+    workSchedule: string[];
+    address: string;
+    phone: string;
+    email: string;
+  };
+};
--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,4 @@ export * from "./about";
 export * from "./brands";
 export * from "./services";
 export * from "./trust";
+export * from "./contacts";
